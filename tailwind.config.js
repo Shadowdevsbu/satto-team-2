700fd7c6a7d468f86cd87fc943ee2dea35098a38
codewithfrancis <offor919@gmail.com>
2025-07-03 20:52:20 +0100

    feat: add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ export default {
     "./index.html",            // for Vite
     "./src/**/*.{js,ts,jsx,tsx}", // for React
   ],
+  colors: {
+    'primary-green': '#00894E',
+    'light-green': '#C7F7C1',
+    'background-gray': '#F5F5F5',
+    'standard-black': '#000000',
+    'dark-text': '#404040',
+    'pure-white': '#FFFFFF',
+  },
   theme: {
     extend: {},
   },
