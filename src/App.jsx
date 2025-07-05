2d158390cf1bc3316e947f8a9d4a20dbd01fe736
codewithfrancis <offor919@gmail.com>
2025-07-05 21:59:35 +0100

    refactor: restructure file and folder organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 // App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard';
+import Dashboard from './pages/student /dashboard';
 import LandingPage from './pages/landing-page';
 import HomeLayout from './pages/layout/HomeLayout';
-import Earnings from './pages/earnings-page';
+import Earnings from './pages/solver/earnings-page';
 
 const App = () => {
   return (
    <Routes>
    <Route path='/' element={<LandingPage />}/>
-   <Route path='/earnings' element={<Earnings />} />
+   <Route path='/earnings' element={<Earnings />} /> 
+
+   {/* Student Routing */}
     <Route path='/dashboard' element={<HomeLayout />}>
     <Route index element={<Dashboard />} />
     </Route>
